b181faa915b459e231eb8fb653ed36cb1bc3bf5b
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2022-03-19 15:26:34 -0300

    main.js ready

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ for (const link of links) {
   });
 }
 
-function changeHeader() {
-  const header = document.querySelector("#header");
-  const navHeight = header.offsetHeight;
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
+function changeHeader() {
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
@@ -36,6 +36,12 @@ const swiper = new Swiper(".swiper", {
   mousewheel: true,
   keyboard: true,
   allowSlideNext: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSizer: true,
+    },
+  },
 });
 
 /*Scroll Reveal*/
@@ -57,13 +63,39 @@ scrollReveal.reveal(
 );
 
 /* Back to top button */
+const backToTopBtn = document.querySelector(".back-to-top");
 
 function backToTop() {
-  const backToTopBtn = document.querySelector(".back-to-top");
   if (window.scrollY >= 560) {
     backToTopBtn.classList.add("show");
   } else {
     backToTopBtn.classList.remove("show");
+  }
+}
+
+/*Ativa menu se visivel na tela*/
+const sections = document.querySelectorAll("main section[id]");
+
+function activeMenu() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
   }
 }
 
