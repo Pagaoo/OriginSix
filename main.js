62d23831753d95059c3fe7ffd3c5b09262def7a4
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2022-03-19 15:40:54 -0300

    bugfixes

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-const nav = document.querySelector("#header nav"); //Procura o id Header e a tag nav e seu atributos, como as classes, salvando na const nav. (Seleciona a primeira que encontrar)
-const toggle = document.querySelectorAll("nav .toggle"); // Aqui seleciona tudo que foi especificado nos (), então, com mais de um, seleciona eles
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
-//Adiciona a class show a tag nav, que faz o menu aparecer na tela, quando clica no 'X' a class show é retirada da tag nav e volta ao normal
 for (const element of toggle) {
   element.addEventListener("click", function () {
     nav.classList.toggle("show");
   });
 }
 
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll("nav ul li a");
-//Ao clique nos links do menu mobile, ira remover a class show da tag nav e redirecionar a página para onde o link clicado leva
+
 for (const link of links) {
   link.addEventListener("click", function () {
     nav.classList.remove("show");
   });
 }
 
+/* mudar o header da página quando der scroll */
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
 
-function changeHeader() {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
     header.classList.add("scroll");
   } else {
+    // menor que a altura do header
     header.classList.remove("scroll");
   }
 }
 
-/*Swiper*/
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   pagination: {
@@ -35,16 +39,15 @@ const swiper = new Swiper(".swiper", {
   },
   mousewheel: true,
   keyboard: true,
-  allowSlideNext: true,
   breakpoints: {
     767: {
       slidesPerView: 2,
-      setWrapperSizer: true,
+      setWrapperSize: true,
     },
   },
 });
 
-/*Scroll Reveal*/
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
   origin: "top",
   distance: "30px",
@@ -53,30 +56,30 @@ const scrollReveal = ScrollReveal({
 });
 
 scrollReveal.reveal(
-  `#home .text, #home .image,
+  `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials,
+  #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links,
-  #footer .brand, #footer .social`,
+  footer .brand, footer .social
+  `,
   { interval: 100 }
 );
 
-/* Back to top button */
-const backToTopBtn = document.querySelector(".back-to-top");
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector(".back-to-top");
 
 function backToTop() {
   if (window.scrollY >= 560) {
-    backToTopBtn.classList.add("show");
+    backToTopButton.classList.add("show");
   } else {
-    backToTopBtn.classList.remove("show");
+    backToTopButton.classList.remove("show");
   }
 }
 
-/*Ativa menu se visivel na tela*/
+/* Menu ativo conforme a seção visível na página */
 const sections = document.querySelectorAll("main section[id]");
-
-function activeMenu() {
+function activateMenuAtCurrentSection() {
   const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
 
   for (const section of sections) {
@@ -99,7 +102,9 @@ function activeMenu() {
   }
 }
 
+/* When Scroll */
 window.addEventListener("scroll", function () {
-  changeHeader();
+  changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
